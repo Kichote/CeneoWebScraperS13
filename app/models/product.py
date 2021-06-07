1526e7a3a700e26217fcd94f61b7619ae1ae4184
Kichote <s219167@student.uek.krakow.pl>
2021-06-07 18:27:01 +0200

    2021-06-07 KS

--- app/models/product.py
+++ app/models/product.py
@@ -11,7 +11,14 @@ class Product:
     def __init__(self, productId, productName=None, opinions=[]) -> None:
         self.productId = productId
         self.productName = productName
-        self.opinions = opinions
+        self.opinions = opinions.copy()
+
+    def extractName(self):
+        respons = requests.get(
+            "https://www.ceneo.pl/{}#tab=reviews".format(self.productId))
+        if respons.status_code == requests.codes.ok:
+            pageDOM = BeautifulSoup(respons.text, 'html.parser')
+            self.productName = extractComponent(pageDOM, '.js_product-h1-link')
 
     def extractProduct(self):
         respons = requests.get("https://www.ceneo.pl/{}#tab=reviews".format(self.productId))
@@ -39,7 +46,6 @@ class Product:
             opinions = product['opinions']
             for opinion in opinions:
                 self.opinions.append(Opinion(**opinion))
-
 
     def toDict(self):
         return {
